fc03b0b5ebc9488b525a1429e7f3b87812875614
awa <2172245494@qq.com>
2026-06-20 22:07:46 +0800

    hummer: Update from COS 11.F.89

---
+++ Android.mk
@@ -0,0 +1,40 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),hummer)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,0fcdfee2e4bdd546b456e02d5e11b42db13176d9)
+$(call add-radio-file-sha1-checked,radio/aop.img,7186dd3aecb97a3e9cc45f9359170309abe5636e)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,77320c69d7f40fb6929669b54b9eccaa7bc8ba65)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,c884eff17086e917b5300288d90ef48b2a7a7769)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,dd0b62e5bf498d951ea5a47c907cacadcc536b5f)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,9d11301030695fa3dddcd9ae3cc86a8a57c2ce78)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,4deda7e19db4b16258e2006903d1a8e72777e5d9)
+$(call add-radio-file-sha1-checked,radio/dsp.img,254db761b8ed878fbb0b2ff930858975c9e07c90)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,932a92f0249d44a881152f3fc92ddd21a57ed69f)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,1904c32fda247d7a3e2054a9ef67a52980382041)
+$(call add-radio-file-sha1-checked,radio/hyp.img,ecd34258cfcb64f3d7f99f6941d9935fbc9bc527)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,4d9790d61a937917166961c794d77b5b227a6544)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,a9c9c33f78a19f9612daed31587da8396398c459)
+$(call add-radio-file-sha1-checked,radio/modem.img,e8f8fd4fbef08d18e142e40428e957ce4a1c7734)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,0f92f5096cb77789a774cbca097068e42b11ffdd)
+$(call add-radio-file-sha1-checked,radio/pdp.img,ccffe6bd0db84161374ec873a6b6abd03e1c638d)
+$(call add-radio-file-sha1-checked,radio/pdp_cdb.img,1fbd01049a9b90cb97705761ea94dccd9b3ccd6b)
+$(call add-radio-file-sha1-checked,radio/pvmfw.img,726fb8eb2be82f02d8f121e36cac45845c553cd6)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,cc8841cca502f8f7999740bef0781dac1de53764)
+$(call add-radio-file-sha1-checked,radio/shrm.img,d9667f323a9069877a27e5b74130604c110459ce)
+$(call add-radio-file-sha1-checked,radio/soccp_dcd.img,294aaa3b30e53dde5c38e070670c8ee4689cacc1)
+$(call add-radio-file-sha1-checked,radio/soccp_debug.img,f181e4d950807ed65494c2b310f115f7ba714875)
+$(call add-radio-file-sha1-checked,radio/splash.img,07e9cda27d7d55806370294443d78ef887e32d2b)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,3e8e7aeda748eec6591cd3bbc5a60559ce9832b7)
+$(call add-radio-file-sha1-checked,radio/tz.img,c3bccc035815a83b39312539ede40cd232f24696)
+$(call add-radio-file-sha1-checked,radio/uefi.img,abf6a0c57f20c0c81d93e29621731012fbe71990)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f4bd47d62e6fc0041754402d9f33a155c9707840)
+$(call add-radio-file-sha1-checked,radio/xbl.img,06750b8b45f0380770fbeaecfd3dacfb8bbae95c)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,4752cb75afdcf3f8c2a0da79c21303531b8e7ae3)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,a7c4f44c42d8f98391937bb97e59830de24a8ee4)
+
+endif
